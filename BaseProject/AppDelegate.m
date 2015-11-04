18b168d2cbd03ee18517fb5a14f32252923e9a4b
BoBo <664026100@qq.com>
2015-11-04 09:13:01 +0800

    一个基础的工程项目
    以后的项目都要基于这个项目    这个是MVVM的project

---
+++ BaseProject/AppDelegate.m
@@ -0,0 +1,23 @@
+//  AppDelegate.m
+//  BaseProject
+//
+//  Created by 廖文博 on 15/10/26.
+//  Copyright © 2015年 wenbo. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "AppDelegate+Category.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    [self initializeWithApplication:application];
+    return YES;
+}
+
+@end
